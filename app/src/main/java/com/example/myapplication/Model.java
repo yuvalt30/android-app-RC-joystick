8585b8f29018301ae9d5405c0207035e31a6aa0a
Yuval Tal <72381398+yuvalt30@users.noreply.github.com>
2021-06-24 15:36:27 +0300

    model better responsibilities seperation and working on threadpool

--- app/src/main/java/com/example/myapplication/Model.java
+++ app/src/main/java/com/example/myapplication/Model.java
@@ -1,37 +1,48 @@
 package com.example.myapplication;
 
-import android.os.StrictMode;
-
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Model {
     Socket fg;
     PrintWriter out;
+    ExecutorService executor;
 
-    public Model(String host, int port) {
+    public Model(String host, int port /*TODO delete parameters*/) {
+        executor =  Executors.newSingleThreadExecutor();
+        connectFG(host, port); // TODO delete this line and detach connection from C-tor
+    }
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        try {
-            fg = new Socket("192.168.43.107", 6400);
-            out = new PrintWriter(fg.getOutputStream(), true);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        setProperty("0", "elevator");
-        setProperty("0", "aileron");
-        setProperty("0", "throttle");
-        setProperty("0", "rudder");
+    public void connectFG(String host, int port){
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    fg = new Socket("172.19.3.158", 6400);
+                    out = new PrintWriter(fg.getOutputStream(), true);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                setProperty("0", "elevator");
+                setProperty("0", "aileron");
+                setProperty("0", "throttle");
+                setProperty("0", "rudder");
+            }
+        });
     }
 
     public void setProperty(String val, String property){
-        String path = "set /controls/flight/"+property;
-        if(property.equals("throttle"))
-            path = "set /controls/engines/current-engine/throttle";
-        out.print(path+" "+val+"\r\n");
-        out.flush();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                String path = "set /controls/flight/"+property;
+                if(property.equals("throttle"))
+                    path = "set /controls/engines/current-engine/throttle";
+                out.print(path+" "+val+"\r\n");
+                out.flush();
+            }
+        });
     }
 }
